72aa714d344ac5931246cf94c5fb806a19fc9e5d
DESKTOP-L6OFII5\halim <hr_blizzard@naver.com>
2024-01-02 21:57:53 +0900

    LocalDateTime 연습
    LocalDateTime 포맷팅이 안되길래 이것저것 실험하면서 추가

---
+++ src/test.java
@@ -0,0 +1,69 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class test {
+    public static void main(String[] args) {
+//        // 현재 날짜와 시간을 가져오기
+//        LocalDateTime now = LocalDateTime.now();
+//
+//        // 형식 지정을 위한 DateTimeFormatter 사용
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+//
+//        // 현재 날짜와 시간을 지정된 형식으로 포맷팅
+//        String formattedDateTime = now.format(formatter);
+//        LocalDateTime current_time = LocalDateTime.parse(now.format(formatter));
+//
+//        System.out.println("Current Date and Time: " + formattedDateTime);
+//        String creation_time = "20240102183640";
+//        LocalDateTime createdTime = LocalDateTime.parse(creation_time, formatter);
+//        LocalDateTime currentTime = LocalDateTime.parse(formattedDateTime, formatter);
+//
+//        System.out.println(createdTime);
+//        System.out.println(currentTime);
+//
+//
+//        LocalDateTime now = LocalDateTime.now();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+//
+//        // 현재 날짜를 "yyyyMMdd" 형식으로 포맷팅
+//        String formattedDate = now.format(formatter);
+//        System.out.println("Current Date: " + formattedDate);
+//
+//        // 형식 지정된 문자열을 다시 LocalDateTime으로 파싱
+//        LocalDateTime parsedDate = LocalDateTime.parse(formattedDate, formatter);
+//        System.out.println("Parsed Date: " + parsedDate);
+//
+//
+//
+//
+//
+//
+//        String creation_time = "20240102183640";
+//        LocalDateTime createdTime = LocalDateTime.parse(creation_time, formatter);
+//        String formattedCreatedTime = createdTime.format(formatter);
+//        System.out.println("Parsed Created Time: " + formattedCreatedTime);
+//        System.out.println(createdTime.getHour());
+//        System.out.println("Time elpased");
+//
+//        Duration elaplsed = Duration.between(createdTime, parsedDate);
+
+        //포맷팅 타입
+        DateTimeFormatter format_type= DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+
+        //now를 뽑고 그걸 parsing하고 daytime으로 바꾸어야함
+        LocalDateTime now = LocalDateTime.now();
+        String now_format_Str = now.format(format_type);
+        LocalDateTime current_time = LocalDateTime.parse(now_format_Str, format_type);
+        System.out.println(current_time);
+
+
+        String creation_time = "20240102215030";
+        LocalDateTime LDT_creation_time = LocalDateTime.parse(creation_time, format_type);
+        System.out.println(LDT_creation_time);
+
+        Duration elapsed_time = Duration.between(LDT_creation_time, current_time);
+        System.out.println(elapsed_time);
+        System.out.println(elapsed_time.getSeconds());
+    }
+}
